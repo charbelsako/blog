fad782d763303801560d7e289816c48ca4568ed4
charbelsako <charbelsarkis82581@gmail.com>
2018-06-16 17:29:43 +0300

    subtle  changes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,19 +1,16 @@
 @extends('layouts.app')
-
 @section('bodyContent')
     <h1>Posts</h1>
     <ul class="collection">
         @if (count($posts) > 0)
             @foreach ($posts as $p)
-                    <li class="collection-item avatar">
-                        <a href="#!"> <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle"> </a>
-                        <a href="#!"><span class="title"> {{ $p->title }} </span> </a>
-                        <p class="truncate">{{$p->body}}</p>
-                        <a href="#!" class="secondary-content"><i class="material-icons">star_border</i></a>
-                    </li>
-                    {{-- <h3> <a href="/posts/{{$p->id}}">{{$p->title}}</a> </h3>
-                    <small>Written On: {{$p->created_at}}</small> --}}
-                
+                <li class="collection-item avatar">
+                    <a href="#!"> <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle"> </a>
+                    <a href="/posts/{{$id}}"><span class="title"> {{ $p->title }} </span> </a>
+                    <p class="truncate">{{$p->body}}</p>
+                    <small>Written On: {{$p->created_at}}</small>
+                    <a href="#!" class="secondary-content"><i class="material-icons">star_border</i></a>
+                </li>
             @endforeach
         @else
             <h3>No posts found.</h3>
