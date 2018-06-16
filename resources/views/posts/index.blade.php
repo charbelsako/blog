4a01a0fe61ad878ea47000d705ab72f88228dc3c
charbelsako <charbelsarkis82581@gmail.com>
2018-06-16 17:27:36 +0300

    modified the layout and views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,14 +2,25 @@
 
 @section('bodyContent')
     <h1>Posts</h1>
-    @if (count($posts) > 0)
-        @foreach ($posts as $p)
-            <div class="">
-                <h3> <a href="/posts/{{$p->id}}">{{$p->title}}</a> </h3>
-                <small>Written On: {{$p->created_at}}</small>
-            </div>
-        @endforeach
-    @else
-        <h3>No posts found.</h3>
-    @endif
+    <ul class="collection">
+        @if (count($posts) > 0)
+            @foreach ($posts as $p)
+                    <li class="collection-item avatar">
+                        <a href="#!"> <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle"> </a>
+                        <a href="#!"><span class="title"> {{ $p->title }} </span> </a>
+                        <p class="truncate">{{$p->body}}</p>
+                        <a href="#!" class="secondary-content"><i class="material-icons">star_border</i></a>
+                    </li>
+                    {{-- <h3> <a href="/posts/{{$p->id}}">{{$p->title}}</a> </h3>
+                    <small>Written On: {{$p->created_at}}</small> --}}
+                
+            @endforeach
+        @else
+            <h3>No posts found.</h3>
+        @endif
+    </ul>
 @endsection
+
+{{-- 
+    
+--}}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,14 +5,16 @@
     <a href="/posts" class="btn-small waves-effect waves-light">
         <i class="material-icons">arrow_back</i>
     </a>
-    <h2>
-        {{$post->title}}
-    </h2>
-    <p>
-        {{$post->body}}
-    </p>    
-    <hr>
-    <small>
-        Written on: {{$post->created_at}}
-    </small>
+    <div style="grey">
+        <h2>
+            {{$post->title}}
+        </h2>
+        <p>
+            {{$post->body}}
+        </p>    
+        <hr>
+        <small>
+            Written on: {{$post->created_at}}
+        </small>
+    </div>
 @endsection
