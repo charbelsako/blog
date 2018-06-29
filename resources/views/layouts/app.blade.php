bfad7442e2c5c408751b6b3eca0ff75154be9e1f
charbelsako <charbelsarkis82581@gmail.com>
2018-06-30 00:33:36 +0300

    subtle changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,19 @@
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {      
+            var elems = document.querySelectorAll('.dropdown-trigger');
+            var instances = M.Dropdown.init(elems);
+        });
+    </script>
+        
 </head>
 <body>
     <div id="app">
@@ -40,27 +52,27 @@
                             <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                     @else
-                    <li class="nav-item dropdown">
-                        <a  class="dropdown-trigger btn" href="#" data-target="dropdown">
-                            {{ Auth::user()->name }} <i class="right material-icons">arrow_drop_down</i>
-                        </a>
-
-                        <ul id="dropdown" class="dropdown-content">
-                            <li><a href="/home">Dashboard</a></li>
-                            <li class="divider" tabindex="-1"></li>
-                            <li><a class="" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                        <li class="">
+                            <a  class="dropdown-trigger btn" href="#" data-target="dropdown">
+                                {{ Auth::user()->name }} <i class="right material-icons">arrow_drop_down</i>
                             </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
+                            <ul id="dropdown" class="dropdown-content">
+                                <li><a href="/home">Dashboard</a></li>
+                                <li class="divider" tabindex="-1"></li>
+                                <li><a class="" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </li>
+                            </ul>
                         </li>
-                        </ul>
-                    </li>
-                @endguest
+                    @endguest
                     
                     
                     
@@ -76,21 +88,10 @@
 
         <main class="py-4 container">
             @include('inc.messages')
-            @yield('bodyContent')
-            
-            
+            @yield('bodyContent')     
         </main>
     </div>
 
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-
-    <script>
-    document.addEventListener('DOMContentLoaded', () => {      
-        var elems = document.querySelectorAll('.dropdown-trigger');
-        var instances = M.Dropdown.init(elems);
-    });
-    </script>
-
+    
 </body>
 </html>
